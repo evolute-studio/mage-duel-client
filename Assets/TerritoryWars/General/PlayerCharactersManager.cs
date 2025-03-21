e1c50c750fbb7b062faa30ec291fdd65cfb78ab3
liyard-tls <mckinley55X@gmail.com>
2025-03-21 12:12:10 +0200

    minor changes

--- Assets/TerritoryWars/General/PlayerCharactersManager.cs
+++ Assets/TerritoryWars/General/PlayerCharactersManager.cs
@@ -5,7 +5,21 @@ namespace TerritoryWars.General
 {
     public class PlayerCharactersManager
     {
-        private static int _currentCharacterId => DojoGameManager.Instance.GetPlayerData(DojoGameManager.Instance.LocalBurnerAccount.Address.Hex()).active_skin;
+        private static int _currentCharacterId
+        {
+            get
+            {
+                bool isLocalBurnerAccountExist = DojoGameManager.Instance.LocalBurnerAccount != null;
+                if (!isLocalBurnerAccountExist)
+                    return 0;
+                bool isPlayerDataExist = DojoGameManager.Instance.GetPlayerData(DojoGameManager.Instance.LocalBurnerAccount.Address.Hex()) != null;
+                if (!isPlayerDataExist)
+                    return 0;
+                return DojoGameManager.Instance.GetPlayerData(DojoGameManager.Instance.LocalBurnerAccount.Address.Hex())
+                    .active_skin;
+            }
+        }
+
         private static int _opponentCharacterId => SessionManager.Instance.IsLocalPlayerHost ? SessionManager.Instance.PlayersData[1].skin_id : SessionManager.Instance.PlayersData[0].skin_id;
         private List<int> _availableCharacters = new List<int> { 0, 1 };
 
