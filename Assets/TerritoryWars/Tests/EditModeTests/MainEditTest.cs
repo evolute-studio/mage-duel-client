933dac09affaae57efe1027f03a9851f840576b6
liyard-tls <mckinley55X@gmail.com>
2025-03-31 19:59:54 +0300

    base testing

---
+++ Assets/TerritoryWars/Tests/EditModeTests/MainEditTest.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class MainEditTest
+{
+    [SetUp]
+    public void Setup()
+    {
+        
+    }
+    
+    // A Test behaves as an ordinary method
+    [Test]
+    public void MainEditTestSimplePasses()
+    {
+    }
+}
