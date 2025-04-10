3119817e4f1b097339de598bcf85bca876090cc2
tokyobladee <124399165+tokyobladee@users.noreply.github.com>
2025-04-10 22:02:32 +0300

    hover panel flag update

--- Assets/TerritoryWars/UI/StructureHoverPanel.cs
+++ Assets/TerritoryWars/UI/StructureHoverPanel.cs
@@ -1,13 +1,16 @@
 using TerritoryWars.General;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace TerritoryWars.UI
 {
     public class StructureHoverPanel : MonoBehaviour
     {
+        public Image Background;
         public TextMeshProUGUI BlueScoreText;
         public TextMeshProUGUI RedScoreText;
+        public Sprite[] ScoreFlags;
         
         public void SetScores(ushort blueScore, ushort redScore)
         {
@@ -21,7 +24,29 @@ namespace TerritoryWars.UI
                 BlueScoreText.text = redScore.ToString();
                 RedScoreText.text = blueScore.ToString();
             }
-            
+            SetFlags(blueScore, redScore);
+        }
+        
+        public void SetFlags(ushort blueScore, ushort redScore)
+        {
+            if (SessionManager.Instance.IsLocalPlayerHost)
+            {
+                if(blueScore == redScore)
+                    Background.sprite = ScoreFlags[0];
+                else if (blueScore > redScore)
+                    Background.sprite = ScoreFlags[1];
+                else
+                    Background.sprite = ScoreFlags[2];
+            }
+            else
+            {
+                if(blueScore == redScore)
+                    Background.sprite = ScoreFlags[0];
+                else if (blueScore < redScore)
+                    Background.sprite = ScoreFlags[1];
+                else
+                    Background.sprite = ScoreFlags[2];
+            }
         }
     }
 }
