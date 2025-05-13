7763d21d42c0b320fe052f256f40e271411cb2e1
liyard-tls <mckinley55X@gmail.com>
2025-05-13 12:33:43 +0300

    animated background

--- Assets/TerritoryWars/UI/Menu/MenuBackground.cs
+++ Assets/TerritoryWars/UI/Menu/MenuBackground.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using DG.Tweening;
+using TerritoryWars.Tools;
 using UnityEngine;
 using UnityEngine.Serialization;
 using UnityEngine.UI;
@@ -7,19 +9,54 @@ namespace TerritoryWars.UI.Menu
 {
     public class MenuBackground : MonoBehaviour
     {
-        public Image DefaultImage;
-        public Image DarkenImage;
+        [SerializeField] private Image DefaultImage;
+        [SerializeField] private Image DarkenImage;
         
-        public Sprite DefaultBackgroundSprite;
-        public Sprite DarkenBackgroundSprite;
+        [SerializeField] private Sprite DefaultBackgroundSprite;
+        [SerializeField] private Sprite DarkenBackgroundSprite;
         
-        public float TransitionTime = 0.5f;
+        [SerializeField] private float TransitionTime = 0.5f;
+        [SerializeField] private float AlphaTarget = 0.5f;
+        
+        [SerializeField] private Transform WindsParent;
+        private List<SpriteAnimator> _winds = new List<SpriteAnimator>();
+        private int _windIndex = 0;
+
+        private void Awake()
+        {
+            for(int i = 0; i < WindsParent.childCount; i++)
+            {
+                var wind = WindsParent.GetChild(i).GetComponent<SpriteAnimator>();
+                if (wind != null)
+                {
+                    _winds.Add(wind);
+                }
+            }
+            InvokeRepeating(nameof(StartWinds), 0, 3f);
+        }
+
+        private void StartWinds()
+        {
+            _windIndex = (_windIndex + 1) % _winds.Count;
+            for (int i = 0; i < _winds.Count; i++)
+            {
+                _winds[i].gameObject.SetActive(i == _windIndex);
+                if (i == _windIndex)
+                {
+                    _winds[i].Play();
+                }
+                else
+                {
+                    _winds[i].Stop();
+                }
+            }
+        }
         
         public void SetBackground(bool darken)
         {
             if (darken)
             {
-                DarkenImage.DOFade(1, TransitionTime);
+                DarkenImage.DOFade(AlphaTarget, TransitionTime);
             }
             else
             {
