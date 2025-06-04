aff6a1823688b3d26cd651d1372cc59f8acb02e5
liyard-tls <mckinley55X@gmail.com>
2025-06-04 14:09:30 +0300

    fixed union find

--- Assets/TerritoryWars/DataModels/UnionFind.cs
+++ Assets/TerritoryWars/DataModels/UnionFind.cs
@@ -29,6 +29,7 @@ namespace TerritoryWars.DataModels
             List<StructureNode> structureNodes = new List<StructureNode>();
             for (int i = 0; i < data.nodes_parents.Length; i++)
             {
+                if (data.nodes_types[i] == (byte)StructureType.None) continue;
                 var (parentPosition, parentSide) = GameConfiguration.GetPositionAndSide(data.nodes_parents[i]);
                 var (position, side) = GameConfiguration.GetPositionAndSide((byte)i);
                 StructureNode node = new StructureNode
@@ -185,8 +186,8 @@ namespace TerritoryWars.DataModels
 
     public enum StructureType
     {
-        None = 0,
-        City = 1,
-        Road = 2,
+        City = 0,
+        Road = 1,
+        None = 2,
     }
 }
